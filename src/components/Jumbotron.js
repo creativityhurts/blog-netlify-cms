992b6133d90e5420d9253e33aec0621b6023f668
rgaengineering <31711386+rgaengineering@users.noreply.github.com>
2018-02-05 22:04:46 +0100

    Update Jumbotron.js

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Jumbotron = ({ title }) => (
   <div className="c-hero section">
     <div className="container">
-      <h2>{title}</h2>
+      <h1>{title}</h1>
     </div>
   </div>
 );
